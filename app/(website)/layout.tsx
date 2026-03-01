073f726ab43a0bb224ee0943ea1f414a4d9611ab
amitchackroborty04 <amitck803@gmail.com>
2026-03-01 12:06:24 +0600

    project setup

---
+++ app/(website)/layout.tsx
@@ -0,0 +1,18 @@
+
+import "../globals.css";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <div>
+        {children}
+      </div>
+    </div>
+  );
+}
