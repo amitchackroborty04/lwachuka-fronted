6be11246a98d516d35ed4fd99df67cd347cec67a
Mamun-Hossain-Dev <mh4559641@gmail.com>
2026-03-06 14:31:16 +0600

    public the env variables

--- lib/api.ts
+++ lib/api.ts
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios from 'axios'
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api/v1',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
-});
+})
 
-export default api;
+export default api
